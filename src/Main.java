31ae4ceb56e30ddc18473b0f1910999702734427
f.mishukov <f.mishukov@gmail.com>
2024-05-05 20:45:40 +0700

    The third home work is almoust done

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,20 @@ import java.io.Console;
 import java.util.Scanner;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Random;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 
 public class Main{
     public static final int coldTemp = -20;
     public static final int warmTemp = -5;
-
     public static void main(String[] args) {
         //Тут я угорел и решил запилить что-то типа меню в main, а все
         //остальное вывести в отдельные функции. Собсна это оно и есть
         //В процессе запила мне пришла мысль, что не плохо бы это все разнести в классы а в
         //main оставить только вызовы клаасов, но вероятность того, что мне хватит времени крайне мала
+        Random random = new Random();
         UseProgramPrint();
         String order;
         boolean useTheProgram = useTheProgram();
@@ -19,49 +23,73 @@ public class Main{
         {
             menuOptionsPrint();
             order = menuNavigation();
-            if ( order.equalsIgnoreCase("calculator") || order.equalsIgnoreCase("Calc"))
+            if ( order.equalsIgnoreCase("1"))
             {
                 calc();
             }
-            else if ((order.equalsIgnoreCase("rainbow")))
+            else if ((order.equalsIgnoreCase("2")))
             {
                 rainbow();
             }
-            else if (order.equalsIgnoreCase("fibonacci"))
+            else if (order.equalsIgnoreCase("3"))
             {
                 fibonacci();
             }
-            else if (order.equalsIgnoreCase("deposit"))
+            else if (order.equalsIgnoreCase("4"))
             {
                 deposit();
             }
-            else if (order.equalsIgnoreCase("SeasonIF"))
+            else if (order.equalsIgnoreCase("5"))
             {
                seasonWithIf();
             }
-            else if (order.equalsIgnoreCase("SeasonSwc"))
+            else if (order.equalsIgnoreCase("6"))
             {
                 seasonWithSwitch();
             }
-            else if (order.equalsIgnoreCase("temperature")|| order.equalsIgnoreCase("temp"))
+            else if (order.equalsIgnoreCase("7"))
             {
                 outsideTemperature();
             }
-            else if (order.equalsIgnoreCase("cycles"))
+            else if (order.equalsIgnoreCase("8"))
             {
                 cycles();
             }
-            else if (order.equalsIgnoreCase("sum")|| order.equalsIgnoreCase("summ"))
+            else if (order.equalsIgnoreCase("9"))
             {
                 sumOfNumbers();
             }
-            else if (order.equalsIgnoreCase("seq")|| order.equalsIgnoreCase("sequence"))
+            else if (order.equalsIgnoreCase("10"))
             {
                 sequence();
             }
-            else if (order.equalsIgnoreCase("table"))
+            else if (order.equalsIgnoreCase("11"))
             {
                 multiplicationTable();
+            }
+            else if (order.equalsIgnoreCase("12"))
+            {
+                removeTheNumberFromArray();
+            }
+            else if (order.equalsIgnoreCase("13"))
+            {
+                ArrayOfEvenNumbers();
+            }
+            else if (order.equalsIgnoreCase("14"))
+            {
+                FindTheNumberInTheArray();
+            }
+            else if (order.equalsIgnoreCase("15"))
+            {
+                replacingAnElementWithOddIndex();
+            }
+            else if (order.equalsIgnoreCase("16"))
+            {
+                ArrayOfEvenNumbersPlus();
+            }
+            else if (order.equalsIgnoreCase("17"))
+            {
+                checkTheMiddle();
             }
             else
             {
@@ -154,17 +182,23 @@ public class Main{
         return (con.readLine());
     }
     public static void menuOptionsPrint() {
-        System.out.println("Type the calculator to use the calculator");
-        System.out.println("Type the rainbow to find out the name of the colour by its number");
-        System.out.println("Type the fibonacci to see the fibonacci sequence");
-        System.out.println("Type the deposit to calculate the deposit income");
-        System.out.println("Type the Seasonif to find out the time of year by month realised by IF");
-        System.out.println("Type the SeasonSwc to find out the time of year by month realised by Switch");
-        System.out.println("Type the temperature to find out is it cold outside or not");
-        System.out.println("Type the cycles to check 3 and 4 tasks");
-        System.out.println("Type the sum to find out the sum to this number");
-        System.out.println("Type the seq to see the sequence (task number 6)");
-        System.out.println("Type the table to see the multiplication table");
+        System.out.println("Type 1 to use the calculator");
+        System.out.println("Type 2 to find out the name of the colour by its number");
+        System.out.println("Type 3 to see the fibonacci sequence");
+        System.out.println("Type 4 to calculate the deposit income");
+        System.out.println("Type 5 to find out the time of year by month realised by IF");
+        System.out.println("Type 6 SeasonSwc to find out the time of year by month realised by Switch");
+        System.out.println("Type 7 to find out is it cold outside or not");
+        System.out.println("Type 8 to check 3 and 4 tasks");
+        System.out.println("Type 9 to find out the sum to this number");
+        System.out.println("Type 10 to see the sequence (task number 6)");
+        System.out.println("Type 11 to see the multiplication table");
+        System.out.println("Type 12 to remove number from for array");
+        System.out.println("Type 13 to see the Array of even numbers");
+        System.out.println("Type 14 to see is the number in the Array");
+        System.out.println("Type 15 Replacing an element of the array with an odd index with 0");
+        System.out.println("Type 16 to see the Array of even number + max and min and middle");
+        System.out.println("Type 17 to combine the middle of two arrays");
     }
 
     //Тупо метод под работу с прогаммой, надо или нет, вызыам то цикла и после, печать так же вынес отдельно
@@ -192,11 +226,9 @@ public class Main{
     {
         int Number;
         Scanner scanner = new Scanner(System.in);
-        if (!scanner.hasNextInt())
-        {
-            System.out.println("You've entered the wrong number");
-            scanner.close();
-            System.exit(1);
+        while (!scanner.hasNextInt()) {  // Проверка, является ли введенное значение целым числом
+            System.out.println("You've entered the wrong number. Please enter a valid integer:");
+            scanner.next();  // Считываем и отбрасываем некорректный ввод, чтобы очистить буфер
         }
         Number = scanner.nextInt();
 
@@ -463,6 +495,296 @@ public class Main{
             {
                 System.out.println(" " + i + "*" + k + " " + i*k);
             }
+        }
+    }
+    //Удаляем элемент из массива
+    public static void removeTheNumberFromArray()
+    {
+        Random random = new Random();
+        System.out.println("Enter the volume of array");
+        int volume = inputIntNumber();
+        int[] badArray = new int[volume];
+        System.out.println("Enter the range of random number that will be entered in the array");
+        System.out.println("Example: if you'll enter 10 - then there will be numbers from 0 to 9");
+        int randomNumber= inputIntNumber();
+        for (int i = 0; i < badArray.length; i++) {
+            badArray[i] = random.nextInt(randomNumber);
+        }
+        //До этого шага все +/- понятно, создали массив, из которого будем удалять
+        //Проиницализировали все, заполнили значениями
+        System.out.println("Enter the number that you need to remove from array");
+        int numberToRemove = inputIntNumber();
+        //Вот тут уже начинается хардкоринка
+        //Завели переменную под флаг, нужно что бы определять были ли в исходном массиве числа которые надо удалить
+        //Завел переменную к - она будет нужна для определения размеров второго массива, куда все сложим.
+        boolean flag = false;
+        int k = 0;
+        for (int i = 0; i < badArray.length; i++)
+        {
+            if (badArray[i] == numberToRemove) {
+                flag = true;
+            }
+            else
+            {
+                k++;
+            }
+        }
+        //А теперь надо создать и принициализировать второй массив с размерностью к
+        //и сложить туда все что нам нужно
+        //При каждой итерации переменная num принимает значение текущего элемента массива badArray, в котором
+        //есть числа которые надо удалить
+        //внутри тела цикла находится условие, которое проверяет, не равен ли текущий элемент num
+        //значению numberToRemove
+        //Записали нужное значение в новый массив а потом переключили на следуюший индекс
+        //+ переделал таким образом, что если НЕ обнаружено вхождений в if наверху, то со вторым массивом не морочимся
+        if (flag == true)
+        {
+            int[] newArrayAfterRemovingTheBadNumber = new int[k];
+            int j = 0;
+            for (int num : badArray)
+            {
+                if (num != numberToRemove)
+                {
+                    newArrayAfterRemovingTheBadNumber[j] = num;
+                    j++;
+                }
+
+            }
+            System.out.println("The bad number was " +numberToRemove + " The array was:");
+            System.out.println(Arrays.toString(badArray));
+            System.out.println("The new array ");
+            System.out.println(Arrays.toString(newArrayAfterRemovingTheBadNumber));
+        }
+        else
+        {
+            System.out.println("There was nothing to remove");
+            System.out.println("The bad number was " +numberToRemove + " The array was:");
+            System.out.println(Arrays.toString(badArray));
+        }
+    }
+    //Массив четных чисел
+    public static void ArrayOfEvenNumbers()
+    {
+        Random random = new Random();
+        int volume;
+        System.out.println("Enter the volume of array between 6 to 10");
+        Scanner scanner = new Scanner(System.in);
+        volume = inputIntNumber();
+        if (volume <=5 || volume >10)
+        {
+            System.out.println("You've entered the wrong number enter the numbers from 6 to 10");
+            scanner.close();
+            System.exit(1);
+        }
+        //Ввели размер массива и добавили проверку на ввод
+        int[] badArray = new int[volume];
+        System.out.println("Enter the range of random number that will be entered in the array");
+        System.out.println("Example: if you'll enter 10 - then there will be numbers from 0 to 9");
+        int randomNumber= inputIntNumber();
+        for (int i = 0; i < badArray.length; i++) {
+            badArray[i] = random.nextInt(randomNumber);
+        }
+        //До этого шага все +/- понятно, создали массив, из которого будем удалять
+        //Проиницализировали все, заполнили значениями
+
+        boolean flag = false;
+        int k = 0;
+        for (int i = 0; i < badArray.length; i++)
+        {
+            if (badArray[i] %2 == 0) {
+                flag = true;
+                k++;
+            }
+        }
+
+        if (flag == true)
+        {
+            int[] newArrayAfterRemovingTheBadNumber = new int[k];
+            int j = 0;
+            for (int num : badArray)
+            {
+                if (num %2 == 0)
+                {
+                    newArrayAfterRemovingTheBadNumber[j] = num;
+                    j++;
+                }
+
+            }
+            System.out.println("Initially the array contained numbers");
+            System.out.println(Arrays.toString(badArray));
+            System.out.println("The new array ");
+            System.out.println(Arrays.toString(newArrayAfterRemovingTheBadNumber));
+        }
+        else
+        {
+            System.out.println("There was no even numbers in the array");
+            System.out.println(Arrays.toString(badArray));
+            System.exit(1);
+        }
+    }
+    //Задача 0 входит ли число в массив или нет
+    public static void FindTheNumberInTheArray()
+    {
+        Random random = new Random();
+        int volume;
+        System.out.println("Enter the volume of array");
+        Scanner scanner = new Scanner(System.in);
+        volume = inputIntNumber();
+        System.out.println("Enter The Number that you want to check");
+        int numberToCheck = inputIntNumber();
+        int[] badArray = new int[volume];
+        System.out.println("Enter the range of random number that will be entered in the array");
+        System.out.println("Example: if you'll enter 10 - then there will be numbers from 0 to 9");
+        int randomNumber= inputIntNumber();
+        for (int i = 0; i < badArray.length; i++) {
+            badArray[i] = random.nextInt(randomNumber);
+        }
+        for (int i = 0; i < badArray.length; i++)
+        {
+            if (badArray[i] == numberToCheck) {
+                System.out.println("We found the number that you are searching for Index of an element in an array " + i +"-"+ badArray[i]);
+            }
+        }
+            System.out.println("Initially the array contained numbers");
+            System.out.println(Arrays.toString(badArray));
+    }
+    //Задача 5/2 замена нечетного индекса на 0
+    public static void replacingAnElementWithOddIndex()
+    {
+        Random random = new Random();
+        int volume;
+        System.out.println("Enter the volume of array");
+        Scanner scanner = new Scanner(System.in);
+        volume = inputIntNumber();
+        int[] badArray = new int[volume];
+        System.out.println("Enter the range of random number that will be entered in the array");
+        System.out.println("Example: if you'll enter 10 - then there will be numbers from 0 to 9");
+        int randomNumber= inputIntNumber();
+        for (int i = 0; i < badArray.length; i++) {
+            badArray[i] = random.nextInt(randomNumber);
+        }
+        System.out.println("Initially the array contained numbers");
+        System.out.println(Arrays.toString(badArray));
+        for (int i = 0; i < badArray.length; i++)
+        {
+            if (i % 2 != 0)
+            {
+                badArray[i] =0;
+            }
+        }
+        System.out.println("The new Array");
+        System.out.println(Arrays.toString(badArray));
+    }
+
+    //Задача 2 на макс, мин и среднее арифмет значение +
+    //Второй массив только из четных
+    public static void ArrayOfEvenNumbersPlus() {
+        Random random = new Random();
+        int volume;
+        System.out.println("Enter the volume of array");
+        Scanner scanner = new Scanner(System.in);
+        volume = inputIntNumber();
+        while (volume<=0)
+        {
+            System.out.println("Enter the volume of array");
+            volume = inputIntNumber();
+        }
+        //Ввели размер массива и добавили проверку на ввод
+        int[] badArray = new int[volume];
+        System.out.println("Enter the range of random number that will be entered in the array");
+        System.out.println("Example: if you'll enter 10 - then there will be numbers from 0 to 9");
+        int randomNumber = inputIntNumber();
+        for (int i = 0; i < badArray.length; i++) {
+            badArray[i] = random.nextInt(randomNumber);
+        }
+        //До этого шага все +/- понятно, создали массив, из которого будем удалять
+        //Проиницализировали все, заполнили значениями
+
+        boolean flag = false;
+        int k = 0;
+        int max = 0;
+        int min = 0;
+        int sum = 0;
+        int counter = 0;
+        double middle = 0;
+        for (int i = 0; i < badArray.length; i++) {
+            counter++;
+            sum = sum + badArray[i];
+            if (badArray[i] > max) {
+                max = badArray[i];
+            }
+            if (badArray[i] < min) {
+                min = badArray[i];
+            }
+
+            if (badArray[i] % 2 == 0) {
+                flag = true;
+                k++;
+            }
+        }
+        middle = sum / counter;
+        if (flag == true) {
+            int[] newArrayAfterRemovingTheBadNumber = new int[k];
+            int j = 0;
+            for (int num : badArray) {
+                if (num % 2 == 0) {
+                    newArrayAfterRemovingTheBadNumber[j] = num;
+                    j++;
+                }
+
+            }
+            System.out.println("Initially the array contained numbers");
+            System.out.println(Arrays.toString(badArray));
+            System.out.println("The new array ");
+            System.out.println(Arrays.toString(newArrayAfterRemovingTheBadNumber));
+            System.out.println("The Maximum is: " + max +" The minimum is: " + " The middle is: " + middle);
+        } else {
+            System.out.println("There was no even numbers in the array");
+            System.out.println("The Maximum is: " + max +" The minimum is: " + min + " The middle is: " + middle);
+            System.out.println(Arrays.toString(badArray));
+            System.exit(1);
+        }
+    }
+
+    //Срвниваем среднее арефмет 2 массивов
+    public static void checkTheMiddle()
+    {
+        Random random = new Random();
+        //Ввели размер массива и добавили проверку на ввод
+        int[] badArray = new int[5];
+        int sum1 = 0;
+        double middle1 = 0;
+        System.out.println("Enter the range of random number that will be entered in the array");
+        System.out.println("Example: if you'll enter 10 - then there will be numbers from 0 to 9");
+        int randomNumber = inputIntNumber();
+        for (int i = 0; i < badArray.length; i++) {
+            badArray[i] = random.nextInt(randomNumber);
+            sum1 = sum1+ badArray[i];
+        }
+        middle1 = sum1/5;
+        int[] goodArray = new int[5];
+        int sum2 = 0;
+        double middle2 = 0;
+        for (int i = 0; i < goodArray.length; i++) {
+            goodArray[i] = random.nextInt(randomNumber);
+            sum2 = sum2+ goodArray[i];
+        }
+        middle2 = sum2/5;
+        System.out.println("The first Array is");
+        System.out.println(Arrays.toString(badArray));
+        System.out.println("The second Array is");
+        System.out.println(Arrays.toString(goodArray));
+        if(middle1 > middle2)
+        {
+            System.out.println("Thr average of first array is bigger "+ middle1 +" > " + middle2);
+        }
+        if(middle1 == middle2)
+        {
+            System.out.println("Thr averages are equal "+ middle1 +" = " + middle2);
+        }
+        if(middle1 < middle2)
+        {
+            System.out.println("Thr average of first array is smaller "+ middle1 +" < " + middle2);
         }
     }
 }
